749f6fe97dff45d9f4542a215f7d436d8e5d8975
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-10-11 13:28:20 +0300

    add connection to server and add box, input and channel componnent

--- frontend/src/pages/PrivatePage.jsx
+++ frontend/src/pages/PrivatePage.jsx
@@ -1,36 +1,63 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import routes from '../routes.js';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { io } from 'socket.io-client';
+import { Row, Container, Col } from 'react-bootstrap';
+import { fetchChatData, actions as channelActions } from '../slices/channelSlice';
+import { actions as messageActios } from '../slices/messageSlice';
+import Channelslist from '../components/channels/ChannelsList';
+import ChannelInfo from '../components/channels/ChannelInfo';
 
-const getAuthHeader = () => {
-  const user = JSON.parse(localStorage.getItem('userInfo'));
-
-  if (user && user.token) {
-    return { Authorization: `Bearer ${user.token}` };
-  }
-
-  return {};
-};
+const socket = io();
 
 const PrivatePage = () => {
-  const [data, setData] = useState('');
+  const dispatch = useDispatch();
+  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { content } = await axios.get(routes.dataPath(), {
-          headers: getAuthHeader(),
-        });
-        setData(content);
-      } catch (error) {
-        <Navigate to="/login" replace />;
-      }
-    };
-    fetchData();
-  }, []);
+    dispatch(fetchChatData());
+  }, [dispatch]);
 
-  return data && <p>{data}</p>;
+  useEffect(() => {
+    socket.on('newMessage', (payload) => {
+      dispatch(messageActios.addMessage(payload));
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    socket.on('newChannel', (payload) => {
+      dispatch(channelActions.addChannel(payload));
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    socket.on('renameChannel', (payload) => {
+      const channelId = payload.id;
+      const newName = payload.name;
+      dispatch(channelActions.renameChannel({ id: channelId, changes: { name: newName } }));
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    socket.on('removeChannel', (payload) => {
+      dispatch(channelActions.removeChannel(payload.id));
+      if (payload.id === currentChannelId) {
+        dispatch(channelActions.setNewId({ id: 1 }));
+      }
+    });
+  }, [currentChannelId, dispatch]);
+
+  return (
+    <Container className="h-100 my-4 overflow-hidden rounded-2 shadow">
+      <Row className="h-100 d-flex flex-md-row bg-white">
+        <Col xs={4} md={2} className="border-end pt-5 px-0 bg-light">
+          <Channelslist />
+        </Col>
+        <Col xs className="h-100 p-0">
+          <ChannelInfo />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default PrivatePage;
