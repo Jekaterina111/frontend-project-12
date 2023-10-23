5de0510ece696ae0ff3f304fe7d91b567de1dcad
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-10-23 16:58:18 +0300

    finish channel components

--- frontend/src/components/channels/Channel.jsx
+++ frontend/src/components/channels/Channel.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
-import { Nav, Button } from 'react-bootstrap';
+import { Button, Dropdown, ButtonGroup } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { actions } from '../../slices/channelSlice';
+import { actions as channelActions } from '../../slices/channelSlice';
+import { actions as modalsActions } from '../../slices/modalsSlice';
+
+const filter = require('leo-profanity');
+
+filter.add(filter.getDictionary('ru'));
+filter.add(filter.getDictionary('en'));
 
 const Channel = ({ channel }) => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
-  const handleClick = () => dispatch(actions.setNewId({ id: channel.id }));
+  const handleClick = () => dispatch(channelActions.setNewId({ id: channel.id }));
+  const removeModal = () => dispatch(modalsActions.openModal({ type: 'removing', channelId: channel.id }));
+  const renameModal = () => dispatch(modalsActions.openModal({ type: 'renaming', channelId: channel.id }));
 
+  const variant = currentChannelId === channel.id ? 'secondary' : '';
   return (
-    <Nav.Item as="li">
-      <Nav.Link as={Button} variant={currentChannelId === channel.id ? 'secondary' : ''} onClick={handleClick} className="text-start rounded-0">
-        <span>#</span>
-        {channel.name}
-      </Nav.Link>
-    </Nav.Item>
+    channel.removable
+      ? (
+        <Dropdown as={ButtonGroup} className="d-flex">
+          <Button
+            variant={variant}
+            className="w-100 text-start rounded-0 text-truncate"
+            onClick={handleClick}
+          >
+            <span className="me-1">#</span>
+            {filter.clean(channel.name)}
+          </Button>
+          <Dropdown.Toggle split variant={variant} id="dropdown-split-basic" className="flex-grow-0">
+            <span className="visually-hidden">{t('modals.buttons.controle_channel')}</span>
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={removeModal}>{t('modals.buttons.remove')}</Dropdown.Item>
+            <Dropdown.Item onClick={renameModal}>{t('modals.buttons.rename')}</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      )
+      : (
+        <Button
+          variant={variant}
+          className="w-100 text-start rounded-0"
+          onClick={handleClick}
+        >
+          <span className="me-1">#</span>
+          {filter.clean(channel.name)}
+        </Button>
+      )
   );
 };
 
--- frontend/src/components/channels/ChannelInfo.jsx
+++ frontend/src/components/channels/ChannelInfo.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import filter from 'leo-profanity';
 import { channelSelectors } from '../../slices/channelSlice';
 import { messageSelectors } from '../../slices/messageSlice';
 import MessagesBox from '../MessagesBox';
 
+filter.add(filter.getDictionary('ru'));
+filter.add(filter.getDictionary('en'));
+
 const ChannelInfo = () => {
   const { t } = useTranslation();
+  const meassageRef = useRef(null);
+
   const { currentChannel, channelMessages } = useSelector((state) => {
     const { currentChannelId } = state.channels;
     const channel = channelSelectors.selectById(state, currentChannelId);
@@ -18,24 +24,34 @@ const ChannelInfo = () => {
     return { currentChannel: channel, channelMessages: messages };
   });
 
+  useEffect(() => {
+    meassageRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, []);
+
+  const messageList = channelMessages.map((message) => (
+    <div className="text-break mb-2" key={message.id}>
+      <b>{`${message.username}: `}</b>
+      {filter.clean(message.body)}
+    </div>
+  ));
+
   return (
-    <div className="small p-3 bg-light shadow-sm">
-      <p className="m-0">
-        <span className="fw-bold">#</span>
-        <span className="fw-bold">
-          {currentChannel && currentChannel.name}
-        </span>
-      </p>
-      <span className="text-muted">
-        {t('chat.number_of_messages', { count: channelMessages.length })}
-      </span>
-      <div id="messages-box" className="chat-messages overflow-auto px-5">
-        {channelMessages.map((sms) => (
-          <div key={sms.id} className="text-break mb-2">
-            <b>{sms.username}</b>
-            {`: ${sms.body}`}
-          </div>
-        ))}
+    <div className="col p-0 h-100">
+      <div className="d-flex flex-column h-100">
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+          <p className="m-0">
+            <span className="fw-bold">#</span>
+            <span className="fw-bold">
+              {currentChannel && filter.clean(currentChannel.name)}
+            </span>
+          </p>
+          <span className="text-muted">
+            {t('chat.number_of_messages', { count: channelMessages.length })}
+          </span>
+        </div>
+      </div>
+      <div id="messages-box" className="chat-messages overflow-auto px-5" ref={meassageRef}>
+        <div className="text-break mt-auto px-5 py-3">{messageList}</div>
       </div>
       {currentChannel && <MessagesBox channelId={currentChannel.id} />}
     </div>
