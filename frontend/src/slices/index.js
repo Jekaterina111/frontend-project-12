785fbd60e848421f3f6316c45f36044cf3d9865c
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-10-23 16:51:25 +0300

    add leo-profanity

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './channelSlice.js';
 import messagesReducer from './messageSlice.js';
+import modalsReducer from './modalsSlice.js';
 
 export default configureStore({
   reducer: {
     channels: channelsReducer,
     messages: messagesReducer,
+    modals: modalsReducer,
   },
 });
