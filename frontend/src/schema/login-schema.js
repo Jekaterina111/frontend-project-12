22bfcc48d6886afda9a3a5564c773051e889b631
Jekaterina111 <sutovic.jekaterina@gmail.com>
2024-04-19 17:18:22 +0300

    add validation errors in schema folder

---
+++ frontend/src/schema/login-schema.js
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+
+export const loginSchema = (text) => {
+   const schema = Yup.object().shape({
+    login: Yup.string()
+      .min(2, 'Минимум 2 буквы')
+      .max(50, 'Максимум 50 букв')
+      .required('Обязательное поле'),
+    password: Yup.string()
+      .min(2, 'Минимум 2 буквы')
+      .max(50, 'Максимум 50 букв')
+      .required('Обязательное поле'),
+  });
+return schema;
+};
